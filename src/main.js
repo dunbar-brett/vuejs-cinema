7dc3d785ca8ae98028c1e9a6e4cfafaa0213f056
dunbar-brett <dunbar.brett@gmail.com>
2018-07-15 22:55:01 -0700

    +basic structure of app +genre list

--- src/main.js
+++ src/main.js
@@ -1,13 +1,52 @@
-let message = 'oh hello'
-let obj = {
-  msg
-};
+import Vue from 'vue';
+import './style.scss';
+import genres from './util/genres';
 
-console.log(msg)
-
-
-
-
-
+new Vue({
+  el: '#app',
+  components: {
+    'movie-list': {
+      template: `<div id="movie-list">
+                  <div v-for="movie in movies" class="movie">{{movie.title}}</div>
+                </div>`,
+      data: function() {
+        return {
+          movies: [
+            {title: 'Pulp Fiction'},
+            {title: 'Home Alone'},
+            {title: 'Austin Powers'}
+          ]
+        }
+      }
+    },
+    'movie-filter': {
+      data() {
+        return {
+          genres
+        }
+      },
+      template: `<div id="movie-filter">
+                  <h2>Filter Results</h2>
+                  <div class="filter-group">
+                    <check-filter v-for="genre in genres" v-bind:title="genre"></check-filter>
+                  </div>
+                </div>`,
+      components: {
+        'check-filter': {
+          data() {
+            return {
+              checked: false
+            }
+          },
+          props: [ 'title' ],
+          template: `<div v-bind:class="{'check-filter': true , active: checked }" v-on:click="checked = !checked">
+                        <span class="checkbox"></span>
+                        <span class="check-filter-title">{{title}}</span>
+                    </div>`
+        }
+      }
+    }
+  }
+})
 
 // save node_modules/.bin/webpack
