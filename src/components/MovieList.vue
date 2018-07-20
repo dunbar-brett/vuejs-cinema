1afb3bc4b8bd6133981f698219af5f981772cbc8
dunbar-brett <dunbar.brett@gmail.com>
2018-07-19 23:29:34 -0700

    broke out components into single files

---
+++ src/components/MovieList.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="movie-list">
+    <div v-for="movie in filteredMovies" class="movie">{{movie.title}}</div>
+  </div>
+</template>
+<script>
+  import genres from '../util/genres';
+  
+  export default {
+      data: function() {
+        return {
+          movies: [
+            {title: 'Kill Bill', genre: genres.CRIME},
+            {title: 'Home Alone', genre: genres.COMEDY},
+            {title: 'Austin Powers', genre: genres.COMEDY}
+          ]
+        }
+      },
+      methods: {
+        moviePassesGenreFilter(movie) {
+          var doesMovieBelong = this.genre.includes(movie.genre);
+          return this.genre.length > 0 ? doesMovieBelong : true;
+        }
+      },
+      computed: {
+        filteredMovies() {
+          return this.movies.filter(movie => this.moviePassesGenreFilter(movie))
+        }
+      },
+      props: ['genre', 'time']
+    } 
+</script>
+<style>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import Vue from 'vue';
 import './style.scss';
-import genres from './util/genres';
+
+import MovieList from './components/MovieList.vue';
+import MovieFilter from './components/MovieFilter.vue';
 
 new Vue({
   el: '#app',
@@ -21,70 +23,8 @@ new Vue({
     }
   },
   components: {
-    'movie-list': {
-      template: `<div id="movie-list">
-                  <div v-for="movie in filteredMovies" class="movie">{{movie.title}}</div>
-                </div>`,
-      data: function() {
-        return {
-          movies: [
-            {title: 'Pulp Fiction', genre: genres.CRIME},
-            {title: 'Home Alone', genre: genres.COMEDY},
-            {title: 'Austin Powers', genre: genres.COMEDY}
-          ]
-        }
-      },
-      methods: {
-        moviePassesGenreFilter(movie) {
-          var doesMovieBelong = this.genre.includes(movie.genre);
-          return this.genre.length > 0 ? doesMovieBelong : true;
-        }
-      },
-      computed: {
-        filteredMovies() {
-          return this.movies.filter(movie => this.moviePassesGenreFilter(movie))
-        }
-      },
-      props: ['genre', 'time']
-    },
-    'movie-filter': {
-      data() {
-        return {
-          genres
-        }
-      },
-      template: `<div id="movie-filter">
-                  <h2>Filter Results</h2>
-                  <div class="filter-group">
-                    <check-filter v-for="genre in genres" v-bind:title="genre" v-on:check-filter="checkFilter"></check-filter>
-                  </div>
-                </div>`,
-      methods: {
-        checkFilter(categorty, title, checked) {
-          this.$emit('check-filter', categorty, title, checked);
-        }
-      },
-      components: {
-        'check-filter': {
-          data() {
-            return {
-              checked: false
-            }
-          },
-          props: [ 'title' ],
-          template: `<div v-bind:class="{'check-filter': true , active: checked }" v-on:click="checkFilter">
-                        <span class="checkbox"></span>
-                        <span class="check-filter-title">{{title}}</span>
-                    </div>`,
-          methods: {
-            checkFilter() {
-              this.checked = !this.checked;
-              this.$emit('check-filter', 'genre', this.title, this.checked);
-            }
-          }
-        }
-      }
-    }
+    MovieList,
+    MovieFilter 
   }
 })
 
